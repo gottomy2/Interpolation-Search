16ec7507acdb5292aa9a754531cfea47779182c1
gottomy2 <68858742+gottomy2@users.noreply.github.com>
2020-12-16 20:45:24 +0100

    info moved to readme

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/**
-    Simple implementation of "Interpolation Search" algorithm,
-    Steps:
-         Step 1 − Start searching data from middle of the list.
-         Step 2 − If it is a match, return the index of the item, and exit.
-         Step 3 − If it is not a match, probe position.
-         Step 4 − Divide the list using probing formula and find the new midle.
-         Step 5 − If data is greater than middle, search in higher sub-list.
-         Step 6 − If data is smaller than middle, search in lower sub-list.
-         Step 7 − Repeat until match.
-    Source: https://www.tutorialspoint.com/data_structures_algorithms/interpolation_search_algorithm.htm
-    @author Igor Motowidło,
-*/
-
 public class Main {
     public static void main(String[] args) {
         Integer[] arr = {5,9,22,31,78,51,321,589,21,91,501,502,503,505};
